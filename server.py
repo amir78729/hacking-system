d130bfe15ef0e69a97c998ccea13a74f4e0c8ca0
Amirhossein Alibakhshi <amirhosseinalibakhshi@gmail.com>
2022-01-03 19:52:53 +0330

    phase2

---
+++ server.py
@@ -0,0 +1,54 @@
+"""Local Server
+
+The origin of this code is from: 
+https://github.com/amir78729/python-TCP-client-server-template/blob/main/Server.py
+"""
+
+import socket
+import threading
+
+
+class Server:
+    def __init__(self):
+        self.PORT = 9999
+        self.MESSAGE_LENGTH_SIZE = 64
+        self.ENCODING = 'utf-8'
+        self.ADDRESS = '127.0.0.1'
+        self.HOST_INFORMATION = (self.ADDRESS, self.PORT)
+
+    def main(self):
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind(self.HOST_INFORMATION)
+        print('[SERVER STARTS] Server is starting...')
+        server.listen()
+        while True:
+            conn, address = server.accept()
+            t = threading.Thread(target=self.handle_client, args=(conn, address))
+            t.start()
+
+    def send_message(self, client, msg):
+        message = msg.encode(self.ENCODING)
+        msg_length = len(message)
+        msg_length = str(msg_length).encode(self.ENCODING)
+        msg_length += b' ' * (self.MESSAGE_LENGTH_SIZE - len(msg_length))
+        client.send(msg_length)
+        client.send(message)
+
+    def handle_client(self, conn, address):
+        print('[NEW CONNECTION] connected from {}.'.format(address))
+        connected = True
+        while connected:
+            try:
+                message_length = int(conn.recv(self.MESSAGE_LENGTH_SIZE).decode(self.ENCODING))
+                msg = conn.recv(message_length).decode(self.ENCODING)
+                print('[MESSAGE RECIEVED] {}'.format(msg))
+                if msg == 'DISCONNECT':
+                    connected = False
+            except ValueError:
+                connected = False
+        conn.close()
+
+
+if __name__ == '__main__':
+    server = Server()
+    server.main()
